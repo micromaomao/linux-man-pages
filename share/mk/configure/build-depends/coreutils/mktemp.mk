829429a8bf7b961a40f147674c429a8d722ecb04
Alejandro Colomar <alx@kernel.org>
2026-08-30 14:45:30 +0200

    share/mk/: $MKTEMP: Add program variable
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ share/mk/configure/build-depends/coreutils/mktemp.mk
@@ -0,0 +1,14 @@
+# Copyright, the authors of the Linux man-pages project
+# SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+
+
+ifndef MAKEFILE_CONFIGURE_BUILD_DEPENDS_COREUTILS_MKTEMP_INCLUDED
+MAKEFILE_CONFIGURE_BUILD_DEPENDS_COREUTILS_MKTEMP_INCLUDED ::= 1
+
+
+ifndef MKTEMP
+MKTEMP ::= mktemp
+endif
+
+
+endif  # include guard
